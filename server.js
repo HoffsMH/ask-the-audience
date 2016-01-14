584cb5c53fb84ee1d5951f670e58da06df4e2f3c
HoffsMH <hecker.matthew@gmail.com>
2016-01-13 23:06:09 -0700

    lernin stuff

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
+"use strict";
+
 const http = require('http');
 const express = require('express');
 
 const app = express();
+
+app.use(function(req, res, next){
+  console.log("hi");
+  res.send({taco: "now"});
+  next();
+});
+// app.use(express.static('public'));
+
+
+let server = http.createServer(app);
+let port = process.env.PORT || 3000;
+
+server.listen(port, function () {
+  console.log('Listening on port ' + port + '.');
+});
