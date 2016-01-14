477a3b9ad89eea30b12efe0342590d48b47ef3ac
HoffsMH <hecker.matthew@gmail.com>
2016-01-14 00:14:41 -0700

    setup file is now changing to ejs for templating engine

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 "use strict";
 
-const express = require('express');
+const express    = require('express');
 const httpserver = require("./httpserver");
-const app = express();
-const socketIo = require('socket.io');
+const app        = express();
+const socketIo   = require('socket.io');
+const setup      = require('./setup')(app);
 
 app.use(express.static('public'));
+
 const server = httpserver.start(app);
 const io = socketIo(server);
 
