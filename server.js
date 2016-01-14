d36d3e5e323586613f0a72b5c765a09577b8d085
HoffsMH <hecker.matthew@gmail.com>
2016-01-14 00:33:55 -0700

    at this point we are now have basic sockets built up time to actually start sending messages

--- server.js
+++ server.js
@@ -11,4 +11,15 @@ app.use(express.static('public'));
 const server = httpserver.start(app);
 const io = socketIo(server);
 
+io.on('connection', function (socket) {
+  console.log('A user has connected.', io.engine.clientsCount);
+
+  io.sockets.emit('usersConnected', io.engine.clientsCount);
+
+  socket.on('disconnect', function () {
+    console.log('A user has disconnected.', io.engine.clientsCount);
+    io.sockets.emit('usersConnected', io.engine.clientsCount);
+  });
+});
+
 module.exports = server;
