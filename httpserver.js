039167f51d4c858b2a35ee0f40328ceeca2e14ef
HoffsMH <hecker.matthew@gmail.com>
2016-01-13 23:33:20 -0700

    back to simplicity for http server setup

---
+++ httpserver.js
@@ -0,0 +1,13 @@
+"use strict";
+const http = require('http');
+
+module.exports = {
+  start(app) {
+    let server = http.createServer(app);
+    let port = process.env.PORT || 3000;
+
+    server.listen(port, function () {
+      console.log('Listening on port ' + port + '.');
+    });
+  }
+}
--- server.js
+++ server.js
@@ -1,22 +1,9 @@
 "use strict";
 
-const http = require('http');
 const express = require('express');
-
+const server = require("./httpserver");
 const app = express();
 
 app.use(express.static('public'));
 
-app.use(function(req, res, next){
-  console.log("hi");
-  res.send({taco: "now"});
-  next();
-});
-
-
-let server = http.createServer(app);
-let port = process.env.PORT || 3000;
-
-server.listen(port, function () {
-  console.log('Listening on port ' + port + '.');
-});
+server.start(app);
